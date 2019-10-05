bdcf8ca73a8fd18f05753e187cc036f6545846ee
Netto Farah <nettofarah@gmail.com>
2019-10-04 17:26:52 -0700

    Fix to use default export

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module '@segment/in-eu' {
-  export function isInEU(): boolean
+  export default function isInEU(): boolean
   export function isInEUTimezone(): boolean
   export function isEULocale(): boolean
 }
